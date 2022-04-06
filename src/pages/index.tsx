dea074a431783955ac62dcbbd2ae7d56c97ffe8a
pedrokpdo <pedromotoehm@gmail.com>
2022-04-06 15:46:28 -0300

    componente Sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function SignIn() {
     >
     <Flex as='form'
           w='100%'
-          maxWidth='360'
+          maxWidth={360}
           bg='gray.800'
           p='8'
           borderRadius={8}
