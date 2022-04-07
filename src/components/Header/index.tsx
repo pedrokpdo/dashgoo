7036b5652509855af6e26d7b65c5070228a9ef6c
pedrokpdo <pedromotoehm@gmail.com>
2022-04-07 13:49:41 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
 
 export function Header () {
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -21,6 +25,20 @@ export function Header () {
             px='6'
             align='center'
         >
+            { !isWideVersion && (
+                <IconButton 
+                aria-label='Open Navigation'
+                icon={<Icon as={RiMenuLine}/>}
+                fontSize='24'
+                variant='unstyled'
+                onClick={onOpen}
+                mr='2'
+                >
+
+                </IconButton>
+
+              
+            ) }
                 <Logo/>
               {isWideVersion && <SearchBox />}
  
@@ -28,7 +46,7 @@ export function Header () {
             align='center'
             ml='auto'
             >
-           
+                
                 <NotificationsNav />
               <Profile showProfileData={isWideVersion}/>
             </Flex>
