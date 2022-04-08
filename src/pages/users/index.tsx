773e7c7758cf1ec7a8f95b9878f98d22fc9943f0
pedrokpdo <pedromotoehm@gmail.com>
2022-04-08 14:21:11 -0300

    config react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,19 +1,29 @@
-import { Text, Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Text, Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import Link from 'next/link'
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useEffect } from "react";
+import { useQuery } from 'react-query'
 
 export default function UserList() {
+        const {data, isLoading, error} = useQuery('users',async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data;
+    })
+
+    
+
     const isWideVersion = useBreakpointValue ({
         base: false,
         lg: true,
     })
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/users').then(response => response.json().then(data => console.log(data)))
+     
     }, [])
 
     return (
@@ -40,7 +50,17 @@ export default function UserList() {
                         </Button>
                       </Link>
                     </Flex>
-                    <Table colorScheme='whiteAlpha'>
+                    { isLoading ? (
+                        <Flex justify='center'>
+                            <Spinner />
+                        </Flex>
+                    ) : error?  (
+                        <Flex justify='center'>
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                    <>
+                        <Table colorScheme='whiteAlpha'>
                         <Thead>
                             <Tr>
                                <Th px={['4', '4', '6']} color='gray.300' width='8'> 
@@ -126,6 +146,8 @@ export default function UserList() {
                         </Tbody>
                     </Table>
                     <Pagination />
+                    </>
+                    ) }
                 </Box>
             </Flex>
         </Box>
